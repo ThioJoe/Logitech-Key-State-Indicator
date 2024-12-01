861786628bd2c97474ab47f6b891269c81f0d9cc
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-11-30 20:07:11 -0800

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Import from Include/LogitechGSDK.cs which uses namespace LedCSharp
+using static LedCSharp.LogitechGSDK;
+
+namespace G915X_KeyState_Indicator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
